3ed64e5d2628c09cb983d3f70cccaab358f25d4c
your new username <smallmonster900@gmail.com>
2024-10-30 20:49:03 +0700

    validate

--- src/main/java/com/ra/controller/CustomerController.java
+++ src/main/java/com/ra/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.ra.controller;
 import com.ra.model.dto.customer.CustomerRequestDTO;
 import com.ra.model.dto.customer.CustomerResponseDTO;
 import com.ra.service.customer.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,7 @@ public class CustomerController {
         return new ResponseEntity<>(responseDTOS, HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody CustomerRequestDTO customerRequestDTO){
+    public ResponseEntity<?> create(@Valid @RequestBody CustomerRequestDTO customerRequestDTO){
         CustomerResponseDTO responseDTO = customerService.create(customerRequestDTO);
         return new ResponseEntity<>(responseDTO,HttpStatus.CREATED);
     }
--- src/main/java/com/ra/model/dto/customer/CustomerRequestDTO.java
+++ src/main/java/com/ra/model/dto/customer/CustomerRequestDTO.java
@@ -1,6 +1,7 @@
 package com.ra.model.dto.customer;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,9 +11,16 @@ import java.time.LocalDate;
 @Setter
 @Builder
 public class CustomerRequestDTO {
+    @NotBlank(message = "Không rỗng")
     private String fullName;
+    @NotBlank(message = "Không rỗng")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Không đúng định dạng email")
     private String email;
+    @NotBlank(message = "Không rỗng")
+    @Size(min = 8,message = "tối thiếu 8 ký tự")
     private String password;
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    @Past(message = "ngày sinh phải là ngày quá khứ")
     private LocalDate birthday;
 }
